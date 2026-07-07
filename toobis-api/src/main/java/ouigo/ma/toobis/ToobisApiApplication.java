616220cfab0b960ab475c073b32834d35f607bbe
missoulghane <m.issoulghane@gmail.com>
2026-07-07 14:23:48 +0200

    Create solution

---
+++ toobis-api/src/main/java/ouigo/ma/toobis/ToobisApiApplication.java
@@ -0,0 +1,13 @@
+package ouigo.ma.toobis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToobisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToobisApiApplication.class, args);
+	}
+
+}
